38900f0b2f3e06dc031b46208e0ebaf5370c6a4a
wogus8285 <wogus8285@gmail.com>
2023-10-23 17:24:20 +0900

    원 연습 종료

--- 연습 a-2(정석)/script/main.js
+++ 연습 a-2(정석)/script/main.js
@@ -1,16 +1,57 @@
 $(function(){
 
-    // 지엔비
-    $('.gnb').hover(function(){
-        // console.log('dd')
-        $(this).find('ul').stop().slideDown()
-    },
-    function(){
-        // console.log('ee')
-        $(this).find('ul').stop().slideUp()
-    },)
-    
+    // <!-- 지엔비 -->
+
+        $('.gnb').hover(function(){
+            // console.log('dd')
+            $('.gnb ul').stop().slideDown()
+
+            $('.header').addClass('active')
+        },
+        
+        
+        function(){
+            // console.log('ee')
+            $('.gnb ul').stop().slideUp()
+
+            $('.header').removeClass('active')
+        })
 
 
+// <!-- 모달 -->
 
+        $('.close_modal').click(function(){
+            $('.modal').hide()},
+            )
+
+        $('.show_modal').click(function(){
+            $('.modal').show()},
+        )
+   
+
+// <!-- 슬라이드 -->
+
+        setInterval(function(){
+            $('.slides').animate({top: '-100%'}, function(){
+                $('.slide').first().appendTo($(this))
+                $(this).css('top', 0)
+            })            
+        }, 3000)
+   
+
+// <!-- 보더 -->
+
+        $('.gallery_list').hide()
+
+        $('.titles h2').click(function(){
+            $('.titles h2').removeClass('active')
+            $(this).addClass('active')
+        
+            var idx = $(this).index()
+
+            $('.item').hide()
+            $('.item').eq(idx).show()
+        })
+
+   
 })
--- 연습 a-3/script/main.js
+++ 연습 a-3/script/main.js
@@ -1,16 +1,16 @@
-$(function(){
+// $(function(){
 
-    // 지엔비
-    $('.gnb').hover(function(){
-        // console.log('dd')
-        $(this).find('ul').stop().slideDown()
-    },
-    function(){
-        // console.log('ee')
-        $(this).find('ul').stop().slideUp()
-    },)
+//     // 지엔비
+//     $('.gnb').hover(function(){
+//         // console.log('dd')
+//         $(this).find('ul').stop().slideDown()
+//     },
+//     function(){
+//         // console.log('ee')
+//         $(this).find('ul').stop().slideUp()
+//     },)
     
 
 
 
-})
+// })
